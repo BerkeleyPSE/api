3071c22be95d3da9cdfea070d90eb7929faea358
Rahul Rangnekar <rrangnekar96@gmail.com>
2018-06-24 22:02:48 -0700

    cors

--- handlers/addCors.js
+++ handlers/addCors.js
@@ -1,9 +1,7 @@
 const cors = require('cors');
 
 module.exports = (req, res, next) => {
-  if (process.env.NODE_ENV === 'development') {
-    cors();
-    res.header('Access-Control-Allow-Origin', '*');
-  }
+  cors();
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 };
